c1c598e8f0bca8400d24689a8eaffa03993276ed
Eric Robsky Huntley <ehuntley@mit.edu>
2023-03-02 16:04:50 -0500

    filing link workflow.

--- R/filing_linkers.R
+++ R/filing_linkers.R
@@ -1,24 +1,13 @@
 source("R/globals.R")
-source("R/log.R")
 source("R/standardizers.R")
 source("R/loaders.R")
+source("R/run_utils.R")
 
-add_parcels_to_assess <- function(town_ids = c(274)) {
-  #' Set up parcels, join asses records to parcel geometry.
-  #'
-  #' @param town_ids List of numerical town ids.
-  #' @returns A dataframe.
-  #' @export
-  parcels <- load_parcels(file.path(DATA_DIR, ASSESS_GDB), town_ids = town_ids) %>% 
-    dplyr::filter(!st_is_empty(geometry)) %>%
-    dplyr::left_join(assess, by = c("loc_id" = "loc_id"))
-}
-
-match_nearby_filings <- function(parcel_points_df, mile_multiplier = 0.1) {
+match_nearby_filings <- function(parcel_points_df, miles = 0.1) {
   #' Match filings to nearby parcels.
   #'
   #' @param parcel_points_df Dataframe containing parcel centroids.
-  #' @param mile_multiplier Bandwidth distance, in miles.
+  #' @param miles Bandwidth distance, in miles.
   #' @returns A dataframe.
   #' @export
   assess_and_filings <- sf::st_join(
@@ -29,97 +18,91 @@ match_nearby_filings <- function(parcel_points_df, mile_multiplier = 0.1) {
       k = 2,
       progress = FALSE) %>% 
     dplyr::filter(!sf::st_is_empty(geometry)) %>% 
-    dplyr::filter(!base::is.na(street))
+    dplyr::filter(!is.na(street))
 }
 
+process_filings <- function(df) {
+  #' Process eviction filings.
+  #'
+  #' @param df A dataframe of eviction filings.
+  #' @returns A dataframe.
+  #' @export
+  df %>%
+    std_flow_strings(c("add1", "city")) %>%
+    std_zip(c("zip")) %>% 
+    std_flow_addresses(c("add1")) %>%
+    std_flow_cities(c("city"))
+}
 
-process_link_filings <- function(town_ids=c(274), mile_multiplier = 0.1) {
+process_link_filings <- function(town_ids = NA, crs = 2249) {
   #' Workflow to connect eviction filings to assessors records.
   #'
   #' @param town_ids List of numerical town ids.
   #' @param mile_multiplier Bandwidth distance, in miles.
   #' @returns A dataframe.
   #' @export
+  assess_file <- file.path(RESULTS_DIR, stringr::str_c(ASSESS_OUT_NAME, "rds", sep = "."))
+  if (file.exists(assess_file)) {
+    log_message("Found previously processed assessors table. Loading...")
+    assess <- readRDS(assess_file)
+  } else {
+    log_message("Loading and processing assessors table...")
+    assess <- load_assess(path = file.path(DATA_DIR, ASSESS_GDB)) %>%
+      process_assess()
+  }
   
-  log_message("Matching filings on text.")
-  assess <- load_assess(path = file.path(DATA_DIR, ASSESS_GDB)) %>%
-    # Run string standardization procedures.
-    std_flow_strings(c("owner1", "own_addr", "site_addr", "own_zip", "city", "own_city")) %>%
-    std_zip(c("zip", "own_zip")) %>% 
-    std_flow_addresses(c("own_addr", "site_addr")) %>%
-    std_flow_cities(c("city", "own_city")) %>%
-    std_flow_names(c("owner1", "own_addr")) %>%
-    # Extract 'care of' entities to co and remove from own_addr.
-    dplyr::mutate(
-      co = dplyr::case_when(
-        stringr::str_detect(
-          own_addr,
-          "C / O"
-        )
-        ~ stringr::str_extract(own_addr, "(?<=C / O ).*$")
-      ),
-      own_addr = dplyr::case_when(
-        stringr::str_detect(
-          own_addr,
-          "C / O"
-        )
-        ~ dplyr::na_if(stringr::str_extract(own_addr, ".*(?= ?C / O )"), ""),
-        TRUE ~ own_addr
-      )
-    ) %>%
-    # Flag owner-occupied properties on the basis of standardized addresses.
-    dplyr::mutate(
-      ooc = dplyr::case_when(
-        own_addr == site_addr ~ TRUE,
-        TRUE ~ FALSE
-      )
-    )
+  log_message("Loading parcels and filtering for residential land use.")
+  parcels <- load_parcels(
+      file.path(DATA_DIR, ASSESS_GDB), 
+      town_ids = town_ids,
+      crs = crs) %>%
+    dplyr::filter(loc_id %in% dplyr::pull(assess, loc_id))
   
-  log_message("Step 1. Join by address strings")
-  
-  filings <- load_filings() %>%
-    std_flow_strings(c("add1", "city")) %>%
-    std_zip(c("zip")) %>% 
-    std_flow_addresses(c("add1")) %>%
-    std_flow_cities(c("city")) %>%
+  log_message("Joining filings to parcels by address and city.")
+  filings <- load_filings(town_ids, crs = crs) %>%
+    process_filings() %>%
     dplyr::left_join(
       dplyr::select(assess, c(loc_id, site_addr, city)), 
       by = c("add1" = "site_addr", "city" = "city")
-      )
-  
-  parcels <- load_parcels(file.path(DATA_DIR, ASSESS_GDB)) %>%
-    dplyr::filter(loc_id %in% pull(assess, loc_id))
+    )
   
   filings_match <- filings %>%
-    dplyr::filter(!is.na(loc_id))
+    dplyr::filter(!is.na(loc_id)) %>%
+    dplyr::mutate(
+      link_type = "address"
+    )
+  
+  filings_unmatchable <- filings %>%
+    dplyr::filter(
+      is.na(loc_id) & 
+        !(match_type %in% c("building", "parcel", "rooftop"))
+    ) %>%
+    dplyr::mutate(
+      link_type = NA_character_
+    )
   
   filings_no_match <- filings %>%
     dplyr::filter(
-      base::is.na(loc_id) & 
+      is.na(loc_id) & 
         match_type %in% c("building", "parcel", "rooftop")
     ) %>%
     dplyr::select(-c(loc_id))
   
   log_message("Step 2. Find assess parcels that contain filings") 
-  filings_results <- filings_no_match %>%
+  filings_no_match %>%
     sf::st_join(parcels, join=sf::st_within) %>%
-    dplyr::bind_rows(filings_match)
-  
-  log_message("-------- Cast parcel geometries to points")
-  assess_points <- sf::st_point_on_surface(assess)
-  
-  log_message(
-    paste("Step 3. Match filings to", mile_multiplier, "miles away from assessor parcel point")
-    ) 
-  nearby_filings <- match_nearby_filings(assess_points, mile_multiplier = mile_multiplier) %>% 
-    sf::st_drop_geometry('geometry')
-  # drop extra geo columns
-  nearby_filings <- nearby_filings[ , !(names(nearby_filings) %in% drops)]
-  # merge last dataframe to big one and write to file
-  df <- dplyr::full_join(df, nearby_filings) %>% 
-  write_delim(
-    file.path(RESULTS_DIR, paste(EVICTORS_OUT_NAME, "csv", sep = ".")),
-    delim = "|", quote = "needed"
-  )
+    dplyr::mutate(
+      link_type = dplyr::case_when(
+        is.na(loc_id) ~ NA_character_,
+        TRUE ~ "spatial"
+        )
+    ) %>%
+    dplyr::bind_rows(filings_match, filings_unmatchable) %>% 
+    sf::st_drop_geometry() %>%
+    dplyr::select(docket, loc_id, link_type) %>%
+    readr::write_delim(
+      file.path(RESULTS_DIR, paste(FILINGS_OUT_NAME, "csv", sep = ".")),
+      delim = "|", quote = "needed"
+    )
 }
 
