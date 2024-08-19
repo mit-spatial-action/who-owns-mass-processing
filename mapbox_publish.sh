1a343517d76391908554781ad4ab30ca4795a512
Eric Robsky Huntley <ericrobskyhuntley@gmail.com>
2024-08-19 12:45:22 -0400

    mapbox_publish shell script

---
+++ mapbox_publish.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# Automate the source creation and tileset publication process.
+
+if ! test -f venv/bin/activate; then
+  echo "Could not locate python virtual environment. Attempting to create."
+  if command -v python &> /dev/null; then
+    python -m venv ./venv
+  elif command -v python3 &> /dev/null; then
+    python3 -m venv ./venv
+  else
+    echo "Could not create virtual environment."
+    exit 1
+  fi
+  source venv/bin/activate
+  python -m pip install 'mapbox-tilesets[estimate-area]'
+else
+  source venv/bin/activate
+fi
+
+
+file=""
+username=""
+tileset=""
+token="${MAPBOX_ACCESS_TOKEN}"
+
+while getopts 'f:u:t:' flag; do
+  case "${flag}" in
+    f) file="${OPTARG}" ;;
+    u) username="${OPTARG}" ;;
+    t) tileset="${OPTARG}" ;;
+    k) token="${OPTARG}" ;;
+  esac
+done
+
+if [[ -z "${token}" ]]; then
+  echo "No Mapbox token found. Either set the MAPBOX_ACCESS_TOKEN environment variable or pass a value using -k."
+  exit 1
+fi
+
+{
+  echo "Uploading data to "$tileset"..."
+  # tilesets upload-source $username --replace --no-validation $tileset $file
+} && {
+  # # If source upload is successful, create tileset.
+  existing_tilesets=$(tilesets list $username)
+  if [[ $existing_tilesets =~ $username"."$tileset ]]; then
+    echo $username"."$tileset" exists. Deleting..."
+    tilesets delete $username"."$tileset
+  fi
+  echo '{
+    "version": 1,
+      "layers": {
+        "geographies": {
+          "source": "mapbox://tileset-source/'$username'/'$tileset'",
+          "minzoom": 10,
+          "maxzoom": 16,
+          "tiles": {
+            "layer_size": 2500
+          }
+        }
+    }
+  }' > recipe.json
+  echo "Creating tileset using recipe..."
+  tilesets create $username"."$tileset --recipe recipe.json --attribution '[
+    {
+      "text": "MIT Spatial Analysis & Action Research Group",
+      "link": "https://github.com/mit-spatial-action/who-owns-mass-processing"
+    }
+  ]' --name "$(echo $tileset | tr "_" " ")"
+  rm recipe.json
+} && {
+  echo "Starting publishing process..."
+  # # If tileset creation is successful, publish tileset.
+  tilesets publish $username"."$tileset
+}
