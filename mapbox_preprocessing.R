dd492e5a493774a3b6a577be0c0da50db6cadd9b
Eric Robsky Huntley <ericrobskyhuntley@gmail.com>
2024-11-08 07:01:53 -0800

    pass aggregation columns to `st_agg_to_grids`

--- mapbox_preprocessing.R
+++ mapbox_preprocessing.R
@@ -12,7 +12,13 @@ st_grid_sf <- function(df, cellsize) {
     tibble::rowid_to_column("grid_id")
 }
 
-st_agg_to_grids <- function(df, cellsizes, centroids=FALSE) {
+st_agg_to_grids <- function(df, 
+                            cellsizes, 
+                            count_col_name,
+                            sum_cols,
+                            mean_cols,
+                            median_cols,
+                            centroids=FALSE) {
   all <- list()
   for (size in cellsizes) {
     grid <- st_grid_sf(points, size)
@@ -21,18 +27,27 @@ st_agg_to_grids <- function(df, cellsizes, centroids=FALSE) {
       sf::st_drop_geometry() |>
       dplyr::group_by(grid_id) |>
       dplyr::summarize(
-        props = dplyr::n(),
-        units = sum(units, na.rm=TRUE),
-        units_mean = mean(unit_count, na.rm=TRUE),
-        prop_mean = mean(prop_count, na.rm=TRUE),
-        units_med = median(unit_count, na.rm=TRUE),
-        prop_med = median(prop_count, na.rm=TRUE)
+        !!count_col_name := dplyr::n(),
+        dplyr::across(
+          {{ sum_cols }},
+          ~ sum(.x, na.rm=TRUE),
+          .names = "{.col}_sum"
+        ),
+        dplyr::across(
+          {{ mean_cols }},
+          ~ mean(.x, na.rm=TRUE),
+          .names = "{.col}_mean"
+        ),
+        dplyr::across(
+          {{ median_cols }},
+          ~ median(.x, na.rm=TRUE),
+          .names = "{.col}_median"
+        )
       ) |>
       dplyr::ungroup() |>
       dplyr::mutate(
         size=size
-      ) |>
-      tidyr::drop_na(units_mean)
+      )
     
     all[[as.character(size)]] <- grid |>
       dplyr::filter(grid_id %in% agg$grid_id) |>
@@ -60,6 +75,63 @@ mapbox_preprocess <- function(load_prefix) {
   } else {
     util_log_message("VALIDATION: Results already present in environment. 🚀🚀🚀")
   }
+  
+  points <- sites_to_owners |>
+    dplyr::left_join(
+      owners |>
+        dplyr::select(-addr_id) |>
+        dplyr::rename(own_name=name) |>
+        dplyr::select(id, own_name, network_group,  inst, trust, trustees),
+      by=dplyr::join_by(owner_id==id)
+    ) |>
+    dplyr::left_join(
+      metacorps_network |>
+        dplyr::select(id, prop_count, unit_count),
+      by=dplyr::join_by(network_group==id)
+    ) |>
+    dplyr::left_join(
+      sites |>
+        dplyr::select(id, ooc),
+      by=dplyr::join_by(site_id==id)
+    ) |>
+    dplyr::filter(
+      !ooc | (unit_count > 1 | prop_count > 1)
+    ) |>
+    dplyr::group_by(
+      site_id
+    ) |>
+    dplyr::arrange(
+      dplyr::desc(prop_count), .by_group=TRUE
+    ) |>
+    dplyr::summarize(
+      owners = stringr::str_flatten_comma(owner_id, na.rm=TRUE),
+      own_name = dplyr::first(own_name),
+      inst = dplyr::first(inst),
+      trust = dplyr::first(trust),
+      trustees = dplyr::first(trustees),
+      network_group = dplyr::first(network_group)
+    ) |>
+    dplyr::ungroup() |>
+    dplyr::left_join(
+      metacorps_network |>
+        dplyr::rename(meta_name=name),
+      by=dplyr::join_by(network_group==id)
+    ) |>
+    dplyr::left_join(
+      sites |>
+        dplyr::select(-muni_id),
+      by=dplyr::join_by(site_id==id)
+    ) |>
+    dplyr::left_join(
+      addresses,
+      by=dplyr::join_by(addr_id==id)
+    ) |>
+    dplyr::left_join(
+      parcels_point |>
+        dplyr::select(loc_id),
+      by=dplyr::join_by(loc_id)
+    ) |>
+    sf::st_set_geometry("geometry")
 }
 
 if (!interactive()) {
@@ -81,67 +153,6 @@ if (!interactive()) {
   mapbox_preprocess(prefix=opt$load_prefix)
 }
 
-
-
-
-  
-points <- sites_to_owners |>
-  dplyr::left_join(
-    owners |>
-      dplyr::select(-addr_id) |>
-      dplyr::rename(own_name=name) |>
-      dplyr::select(id, own_name, network_group,  inst, trust, trustees),
-    by=dplyr::join_by(owner_id==id)
-  ) |>
-  dplyr::left_join(
-    metacorps_network |>
-      dplyr::select(id, prop_count, unit_count),
-    by=dplyr::join_by(network_group==id)
-  ) |>
-  dplyr::left_join(
-    sites |>
-      dplyr::select(id, ooc),
-    by=dplyr::join_by(site_id==id)
-  ) |>
-  dplyr::filter(
-    !ooc | (unit_count > 1 | prop_count > 1)
-  ) |>
-  dplyr::group_by(
-    site_id
-  ) |>
-  dplyr::arrange(
-    dplyr::desc(prop_count), .by_group=TRUE
-  ) |>
-  dplyr::summarize(
-    owners = stringr::str_flatten_comma(owner_id, na.rm=TRUE),
-    own_name = dplyr::first(own_name),
-    inst = dplyr::first(inst),
-    trust = dplyr::first(trust),
-    trustees = dplyr::first(trustees),
-    network_group = dplyr::first(network_group)
-  ) |>
-  dplyr::ungroup() |>
-  dplyr::left_join(
-    metacorps_network |>
-      dplyr::rename(meta_name=name),
-    by=dplyr::join_by(network_group==id)
-  ) |>
-  dplyr::left_join(
-    sites |>
-      dplyr::select(-muni_id),
-    by=dplyr::join_by(site_id==id)
-  ) |>
-  dplyr::left_join(
-    addresses,
-    by=dplyr::join_by(addr_id==id)
-  ) |>
-  dplyr::left_join(
-    parcels_point |>
-      dplyr::select(loc_id),
-    by=dplyr::join_by(loc_id)
-  ) |>
-  sf::st_set_geometry("geometry")
-
 t <- points |>
   dplyr::mutate(
     quartile = dplyr::ntile(prop_count, 4),
@@ -149,7 +160,7 @@ t <- points |>
   ) |>
   dplyr::select(site_id, network_group, owners, loc_id, addr, muni, quartile, quintile) |>
   sf::st_transform(4326) |>
-  sf::st_write('mapbox_points.shp', delete_dsn=TRUE)
+  sf::st_write('mapbox_points.geojson', delete_dsn=TRUE)
 
   
 t <- st_agg_to_grids(
@@ -158,10 +169,14 @@ t <- st_agg_to_grids(
       units::as_units(0.25, "miles"), 
       units::as_units(0.5, "miles")
     ),
+    count_col_name = "props",
+    sum_cols=c(units),
+    mean_cols=c(unit_count, prop_count),
+    median_cols=c(unit_count, prop_count),
     centroids=FALSE
     ) 
 
 t |>
   sf::st_transform(4326) |>
-    sf::st_write("summary_hex.geojson", delete_dsn=TRUE)
+    sf::st_write("mapbox_hexes.geojson", delete_dsn=TRUE)
   
